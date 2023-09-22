86ed31d442fd0746b096121abf872607f5ce2561
Akash Chauhan <akash.th4kur@gmail.com>
2023-09-22 23:18:14 +0530

    commented unused code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
-const bodyParser = require("body-parser");
+// const MongoClient = require("mongodb").MongoClient;
+// const bodyParser = require("body-parser");
 const app = express();
 
 const port = 8000;
