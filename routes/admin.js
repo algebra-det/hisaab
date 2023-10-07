e34f74c06e33b492b45b0aae05cff70e4c52887a
Akash Chauhan <akash.th4kur@gmail.com>
2023-10-07 14:40:18 +0530

    text update

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
-  res.send({ message: "Admin Route" });
+  res.send({ message: "Home Admin Route" });
 });
 
 module.exports = router;
