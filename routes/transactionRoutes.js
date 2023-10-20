d0f06bac205fad840bd815272689a9846f5e30c4
Akash Chauhan <akash.th4kur@gmail.com>
2023-10-20 15:47:40 +0530

    Same route for transactions post/get

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const homeController = require("../controller/transactionController");
 
 router.get("/", homeController.getTransactions);
-router.post("/create", homeController.createTransaction);
+router.post("/", homeController.createTransaction);
 
 module.exports = router;
