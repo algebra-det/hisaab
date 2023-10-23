aa63af2e6b77be82dd42abc8d56b3de367b890c6
Akash Chauhan <akash.th4kur@gmail.com>
2023-10-23 22:08:07 +0530

    text message on index route updated

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,7 +1,5 @@
 const indexController = async (req, res, next) => {
-  return res
-    .status(200)
-    .json({ message: "Comming Soon. List Your Transactions." });
+  return res.status(200).json({ message: "Comming Soon." });
 };
 
 module.exports = {
