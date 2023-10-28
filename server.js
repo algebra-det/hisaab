34e37f9da3bf5bb3710ba52b9118ddff47814295
Akash Chauhan <akash.th4kur@gmail.com>
2023-10-28 20:24:46 +0530

    corsConfig removed

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ authenticateDB();
 
 // For parsing application/json
 app.use(express.json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
