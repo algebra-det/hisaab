282c585c7574eabce609af69d6bf0a0b2ea634aa
Akash Chauhan <akash.th4kur@gmail.com>
2023-11-13 23:56:28 +0530

    added hook to save product lastSellingPrice

--- database/transactionHooks/index.js
+++ database/transactionHooks/index.js
@@ -1,24 +1,28 @@
-const Product = require("../../models/Product");
+const Product = require('../../models/Product')
 
-const createProduct = async (transaction) => {
+const createProduct = async transaction => {
   await Product.create({
     productName: transaction.productName,
     purchasePrice: transaction.purchasePrice,
+    lastSellingPrice: transaction.sellingPrice,
     createdBy: transaction.createdBy,
-  });
-};
+  })
+}
 
-const createOrUpdateProducts = async (transaction) => {
+const createOrUpdateProducts = async transaction => {
   try {
     const product = await Product.findOne({
       where: { productName: transaction.productName },
-    });
-    if (!product) createProduct(transaction);
+    })
+    if (!product) createProduct(transaction)
     else if (product.purchasePrice !== transaction.purchasePrice)
-      product.update({ purchasePrice: transaction.purchasePrice });
+      product.update({
+        purchasePrice: transaction.purchasePrice,
+        lastSellingPrice: transaction.sellingPrice,
+      })
   } catch (error) {
-    console.log("Transaction Hook Error: ", error);
+    console.log('Transaction Hook Error: ', error)
   }
-};
+}
 
-module.exports = createOrUpdateProducts;
+module.exports = createOrUpdateProducts
