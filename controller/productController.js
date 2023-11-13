37992894bec8f2e3137be6e3aa7ccaf9263cbbbf
Akash Chauhan <akash.th4kur@gmail.com>
2023-11-14 00:21:48 +0530

    searchText fixes for productSearch

--- controller/productController.js
+++ controller/productController.js
@@ -1,47 +1,48 @@
-const Product = require("../models/Product");
-const { Op } = require("sequelize");
+const Product = require('../models/Product')
+const { Op } = require('sequelize')
 
 const getMyProducts = async (req, res, next) => {
-  let { limit, offset } = req.query;
-  if (!offset) offset = 0;
-  if (!limit) limit = 10;
+  let { limit, offset } = req.query
+  if (!offset) offset = 0
+  if (!limit) limit = 10
   const { count, rows } = await Product.findAndCountAll({
     offset,
     limit,
     where: {
       createdBy: req.user.id,
     },
-    order: [["updatedAt", "DESC"]],
-  });
+    order: [['updatedAt', 'DESC']],
+  })
   res.json({
-    message: "Results fetched successfully",
+    message: 'Results fetched successfully',
     data: rows,
     count,
-  });
-};
+  })
+}
 
 const getProductsViaSearch = async (req, res, next) => {
-  let { searchText } = req.query;
-  if (!searchText || searchText.length <= 2)
+  let { searchText } = req.query
+  if (!searchText || typeof searchText !== 'string' || searchText.length <= 2)
     return res.status(400).json({
       message:
         "Please pass a text string and it's length should be atleast 3 characters",
-    });
+    })
+  console.log('To LowerCase: ', searchText, searchText.toLowerCase())
   const data = await Product.findAll({
     where: {
       createdBy: req.user.id,
       productName: {
-        [Op.substring]: searchText,
+        [Op.iLike]: '%' + searchText + '%',
       },
     },
-  });
+  })
   res.json({
-    message: "Results fetched successfully",
+    message: 'Results fetched successfully',
     data,
-  });
-};
+  })
+}
 
 module.exports = {
   getMyProducts,
   getProductsViaSearch,
-};
+}
