a870cdfb4cee8441108a96509e1ab0d8dd85fe46
Akash Chauhan <akash.th4kur@gmail.com>
2024-02-08 16:54:59 +0530

    Refactored transactionController

--- controller/transactionController.js
+++ controller/transactionController.js
@@ -1,83 +1,25 @@
 const Transaction = require('../models/Transaction')
 const { Op } = require('sequelize')
 const sequelize = require('../database')
-const getFilterDataFromRequest = require('../helpers/getFilterDataFromRequest')
+const { getFilterDataFromRequest } = require('../helpers')
+const { asyncHandler, ErrorResponse, ApiResponse } = require('../utils')
 
-const getInstance = async (req, res) => {
+const getInstance = async (req) => {
   const { id } = req.params
-  if (!id)
-    return res.status(400).json({
-      message: 'Transaction ID is required'
-    })
+  if (!id) throw new ErrorResponse(400, 'Transaction ID is required')
   const transaction = await Transaction.findByPk(id)
-  if (!transaction)
-    return res.status(400).json({
-      message: 'No transaction found.'
-    })
+  if (!transaction) throw new ErrorResponse(400, 'No transaction found.')
   if (transaction.createdBy !== req.user.id)
-    return res.status(400).json({
-      message: 'Not authorized to update such transactions'
-    })
+    throw new ErrorResponse(400, 'Not authorized to update such transactions')
   return transaction
 }
 
-const getTransactions = async (req, res, _next) => {
-  try {
-    const { startTime, endTime, ordering, offset, limit } =
-      getFilterDataFromRequest(req, res)
+const getTransactions = asyncHandler(async (req, res, _next) => {
+  const { startTime, endTime, ordering, offset, limit } =
+    getFilterDataFromRequest(req, res)
 
-    const [totalProfit, { rows, count }] = await Promise.all([
-      Transaction.findAll({
-        where: {
-          createdAt: {
-            [Op.gte]: startTime,
-            [Op.lte]: endTime
-          },
-          createdBy: req.user.id
-        },
-        attributes: [
-          [sequelize.fn('SUM', sequelize.col('profit')), 'totalProfit']
-        ]
-      }),
-      Transaction.findAndCountAll({
-        limit,
-        offset,
-        where: {
-          createdAt: {
-            [Op.gte]: startTime,
-            [Op.lte]: endTime
-          },
-          createdBy: req.user.id
-        },
-        order: [ordering]
-      })
-    ])
-    return res.json({
-      message: 'Fetched Successfuly',
-      data: rows,
-      totalProfit: totalProfit[0].dataValues.totalProfit,
-      count,
-      startTime,
-      endTime
-    })
-  } catch (error) {
-    console.log('Error while fetching transactions: ', error)
-    return res.status(400).json({
-      message: 'Something went wrong',
-      error
-    })
-  }
-}
-
-const transactionStats = async (req, res) => {
-  try {
-    const { startTime, endTime, offset, limit } = getFilterDataFromRequest(
-      req,
-      res
-    )
-    let totalProfit = await Transaction.findAll({
-      limit,
-      offset,
+  const [totalProfit, { rows, count }] = await Promise.all([
+    Transaction.findAll({
       where: {
         createdAt: {
           [Op.gte]: startTime,
@@ -88,61 +30,79 @@ const transactionStats = async (req, res) => {
       attributes: [
         [sequelize.fn('SUM', sequelize.col('profit')), 'totalProfit']
       ]
+    }),
+    Transaction.findAndCountAll({
+      limit,
+      offset,
+      where: {
+        createdAt: {
+          [Op.gte]: startTime,
+          [Op.lte]: endTime
+        },
+        createdBy: req.user.id
+      },
+      order: [ordering]
     })
-    res.json({
-      message: 'Fetched Successfuly',
-      totalProfit: totalProfit[0].dataValues.totalProfit,
-      startTime,
-      endTime
-    })
-  } catch (error) {
-    console.log('stats error: ', error)
-    res.status(400).json({
-      message: 'Something went wrong',
-      error
-    })
+  ])
+  const data = {
+    rows,
+    totalProfit: totalProfit[0].dataValues.totalProfit,
+    count,
+    startTime,
+    endTime
   }
-}
+  res.json(new ApiResponse(data, 'Fetched Successfuly'))
+})
 
-const createTransaction = async (req, res, _next) => {
-  try {
-    const { productName, purchasePrice, sellingPrice } = req.body
-    const data = await Transaction.create({
-      productName,
-      purchasePrice,
-      sellingPrice,
+const transactionStats = asyncHandler(async (req, res) => {
+  const { startTime, endTime, offset, limit } = getFilterDataFromRequest(
+    req,
+    res
+  )
+  let totalProfit = await Transaction.findAll({
+    limit,
+    offset,
+    where: {
+      createdAt: {
+        [Op.gte]: startTime,
+        [Op.lte]: endTime
+      },
       createdBy: req.user.id
-    })
-    res.json({ message: 'created Successfuly', data })
-  } catch (error) {
-    console.log('error occured: ', error)
-    res.status(500).send({ message: 'Failed', error })
+    },
+    attributes: [[sequelize.fn('SUM', sequelize.col('profit')), 'totalProfit']]
+  })
+  const data = {
+    totalProfit: totalProfit[0].dataValues.totalProfit,
+    startTime,
+    endTime
   }
-}
+  res.json(new ApiResponse(data, 'Fetched Successfuly'))
+})
 
-const updateTransaction = async (req, res) => {
-  try {
-    const body = req.body
-    delete body['id']
-    const data = await getInstance(req, res)
-    await data.update({ ...body })
-    res.json({ message: 'Updated successfully', data })
-  } catch (error) {
-    console.log('Error occured while deleting: ', error)
-    res.status(500).send({ message: 'Failed', error })
-  }
-}
+const createTransaction = asyncHandler(async (req, res, _next) => {
+  const { productName, purchasePrice, sellingPrice } = req.body
+  const data = await Transaction.create({
+    productName,
+    purchasePrice,
+    sellingPrice,
+    createdBy: req.user.id
+  })
+  res.json(new ApiResponse(data, 'created Successfuly'))
+})
 
-const deleteTransaction = async (req, res) => {
-  try {
-    const transaction = await getInstance(req, res)
-    transaction.destroy()
-    res.json({ message: 'Deleted successfully' })
-  } catch (error) {
-    console.log('Error occured while deleting: ', error)
-    res.status(500).send({ message: 'Failed', error })
-  }
-}
+const updateTransaction = asyncHandler(async (req, res) => {
+  const body = req.body
+  delete body['id']
+  const data = await getInstance(req, res)
+  await data.update({ ...body })
+  res.json(new ApiResponse(data, 'updated successfully'))
+})
+
+const deleteTransaction = asyncHandler(async (req, res) => {
+  const transaction = await getInstance(req, res)
+  await transaction.destroy()
+  res.status(204).json(new ApiResponse({}, 'Deleted successfully', 204))
+})
 
 module.exports = {
   getTransactions,
